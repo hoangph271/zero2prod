cee14af9ef6a349a4baecbf9ea912a92dab0a639
Phan Huy Hoang <hoangph271@gmail.com>
2024-12-14 03:40:26 +0700

    add GET /health_check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer};
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(|| HttpResponse::Ok())))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
